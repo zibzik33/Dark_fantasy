6d10b360c3d9c1527eda9bc66557f48d7d647f57
zahar <zaharionkin@gmail.com>
2025-05-10 19:45:34 +0300

    update 2
    поменял язык в комментариях

--- Data.hpp
+++ Data.hpp
@@ -1,43 +1,43 @@
 #include "includes.hpp"
 
-// класс для создания игрока 
+// class for make player
 class Player
 {
-// приватно
+// private
 private:
 	std::string Name;
 	int MaxHealth;
 	int Health;
 	int Damage;
 	bool is_died = false;
-// публично
+// public
 public:
-	// конструктор
+	// constructor
 	Player(int _health, std::string _name) {
 		MaxHealth = _health;
 		Name = _name;
 		Damage = 1;
 		Health = _health;
 	}
-	// получить значение здоровья игрока
+	// get the player's health value
 	int GetHealth()
 	{
 		std::cout << "Your health : " << Health << '\n' << "Your maximal health : " << MaxHealth << '\n';
 		return Health;
 	}
-	// получить значение урона игрока
+	// get player damage value
 	int GetDamage()
 	{
 		std::cout << "Your damage : " << Damage << '\n';
 		return Damage;
 	}
-	// получить имя игрока
+	// get player name
 	std::string GetName()
 	{
 		std::cout << "Your name : " << Name << '\n';
 		return Name;
 	}
-	// восстановление здоровья 
+	// restoration of health
 	int Healing(int value)
 	{
 		Health += value;
@@ -49,19 +49,19 @@ public:
 		std::cout << "You healing on " << value << "hits\n";
 		return Health;
 	}
-	// вражеский урон
+	// enemy damage
 	void EnemyDamage(int _enemyDamage)
 	{
 		Health -= _enemyDamage;
 		CheckDied();
 		DiedMessage();
 	}
-	// вывести статистики игрока 
+	// display player statistics 
 	void GetStat()
 	{
 		std::cout << "You have : \nName : " << Name << "\nHealth - " << Health << '/' << MaxHealth << "\nDamage - " << Damage << '\n';
 	}
-	// задать значение максимального здоровья
+	// set max health value
 	void SetMaxHealth(int _mhealth)
 	{
 		MaxHealth = _mhealth;
@@ -72,7 +72,7 @@ public:
 		CheckDied();
 		DiedMessage();
 	}
-	// проверить умер ли игрок
+	// check if the player is dead
 	bool CheckDied()
 	{
 		if (Health > 0)
@@ -82,7 +82,7 @@ public:
 		is_died = true;
 		return is_died;
 	}
-	// сообщение о смерти
+	// death message
 	void DiedMessage()
 	{
 		if (is_died)
